d66d50d6aa74cc8fd159f1a732632ee346dd3d99
Antonin Folliasson <antonin@aitio.co>
2022-08-12 12:17:40 +0200

    initial commit before starting

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Bell from './bell.svg'
+import avatar from "./img/avatar.png"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Full Page</h1>
+      <Bell />
+      <img src={avatar} />
+    </div>
+  )
+
+}
+
+export default App
